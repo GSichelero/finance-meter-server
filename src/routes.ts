216b2028f1aaca4eaacc4ead117a7c35e6ea8028
GSichelero <gabriel.sichelero@hotmail.com>
2022-07-21 10:20:03 -0300

    init server

---
+++ src/routes.ts
@@ -0,0 +1,197 @@
+import express from 'express'
+const bodyParser = require('body-parser');
+var googleFinance = require('google-finance');
+const { Client } = require('pg')
+const multer = require('multer');
+var XLSX = require('node-xlsx');
+const yahooFinance = require('yahoo-finance2').default;
+
+var manage_data = require('./utils/manage_data');
+
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'financedb',
+  password: 'ggi2011',
+  port: 5432,
+})
+client.connect()
+
+// if table finance_data does not exist, create it
+// finance_data is the table name, and the columns are: date, movement, product, quantity, unit_price, operation_value
+client.query('CREATE TABLE IF NOT EXISTS finance_data (date date, movement varchar(100), product varchar(100), quantity numeric(10, 2), unit_price numeric(10, 2), operation_value numeric(10, 2))')
+
+const routes = express()
+
+// routes.use(bodyParser.urlencoded({ extended: true }))
+routes.use(express.json({limit: '50mb'}));
+routes.use(express.urlencoded({limit: '50mb'}));
+
+// routes.use(routes.router)
+
+routes.get('/', (request, response) => {
+  // select all rows from finance_data
+  client.query('SELECT * FROM finance_data', (err: any, res: any) => {
+    if (err) {
+      console.log(err.stack)
+    } else {
+      console.log(res.rows)
+    }
+  })
+  return response.json({ message: 'Hello World' })
+})
+
+
+routes.get('/stock/:name', async function(request, response) {
+  let queryOptions = { period1: '2022-02-01', period2: '2022-03-01'};
+  try {
+    let quotes = await yahooFinance.historical(request.params.name, queryOptions);
+    response.json(quotes);
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+
+routes.get('/get-financial-data', async function(request, response) {
+  return response.json(manage_data.filterData(await manage_data.getFinancialData()));
+})
+
+
+routes.get('/get-financial-dividends', async function(request, response) {
+  return response.json(await manage_data.getFinancialDividends());
+})
+
+
+// save more than one file temporarily in disk
+const upload = multer({ dest: 'uploads/' })
+routes.post('/upload-report-file', upload.array('reports'), async function(request: any, response: any) {
+  let firstDate = new Date(9999, 1, 1);
+  let lastDate = new Date(0);
+  let firstDateQuery = await client.query('SELECT date FROM finance_data ORDER BY date ASC LIMIT 1');
+  if (firstDateQuery.rows.length > 0) {
+    firstDate = firstDateQuery.rows[0].date;
+    let lastDateQuery = await client.query('SELECT date FROM finance_data ORDER BY date DESC LIMIT 1');
+    if (lastDateQuery.rows.length > 0) {
+      lastDate = lastDateQuery.rows[0].date;
+    }
+  }
+  
+  for (let file of request.files) {
+    const workSheetsFromFile = XLSX.parse(file.path);
+    for (let row of workSheetsFromFile[0].data) {
+      if (row[1] != 'Data') {
+        if (row[2] == 'Compra' || row[2] == 'Dividendo' || row[2] == 'Juros Sobre Capital Próprio' || row[2] == 'Rendimento' || row[2] == 'Transferência - Liquidação' || row[2] == 'Desdobro' || row[2] == 'Atualização') {
+          row[1] = row[1].split('/').reverse().join('-');
+          let dateObject = new Date(row[1]);
+          if (dateObject < firstDate || dateObject > lastDate) {
+            if (row[6] == '-') {
+              row[6] = 0;
+            }
+            if (row[7] == '-') {
+              row[7] = 0;
+            }
+            row[5] = row[5].replace(',', '.');
+            await client.query('INSERT INTO finance_data (date, movement, product, quantity, unit_price, operation_value) VALUES ($1, $2, $3, $4, $5, $6)', [row[1], row[2], row[3], row[5], row[6], row[7]])
+          }
+        }
+      }
+    }
+  }
+
+  // select the products, quantity that has movement Desdobro
+  let products_with_desdobro = await client.query('SELECT product, quantity FROM finance_data WHERE movement = \'Desdobro\'');
+  products_with_desdobro = products_with_desdobro.rows;
+  if (products_with_desdobro.length > 0) {
+    // for each product, get the sum of quantity when movement is Transferência - Liquidação
+    let products_with_desdobro_with_quantity = await Promise.all(products_with_desdobro.map(async function(item: any) {
+      let quantity = await client.query('SELECT SUM(quantity) FROM finance_data WHERE movement = \'Transferência - Liquidação\' AND product = $1::text', [item.product]);
+      quantity = quantity.rows[0].sum;
+      return {
+        product: item.product,
+        quantity: quantity
+      }
+    }));
+
+    let new_quantity_products = products_with_desdobro_with_quantity.map(function(item: any) {
+      let desdobro_quantity = products_with_desdobro.find(function(item2: any) {return item2.product === item.product}).quantity;
+      let new_quantity_math = Number((Number(item.quantity) + Number(desdobro_quantity)) / Number(item.quantity));
+      return {
+        product: item.product,
+        new_quantity: new_quantity_math
+      }
+    });
+
+    // for product in new_quantity_products, update the quantity to quantity multiplied by new_quantity and unit_price to unit_price divided by new_quantity and operation_value to operation_value divided by new_quantity in finance_data
+    await Promise.all(new_quantity_products.map(async function(item: any) {
+      // select the unit_price, quantity and operation_value of the product
+      let product_data = await client.query('SELECT unit_price, quantity, operation_value, date FROM finance_data WHERE product = $1', [item.product]);
+      for (let row of product_data.rows) {
+        let new_unit_price = Number(row.unit_price) / Number(item.new_quantity);
+        let new_operation_value = Number(row.operation_value) / Number(item.new_quantity);
+        let new_quantity = Number(row.quantity) * Number(item.new_quantity);
+        await client.query('UPDATE finance_data SET quantity = $1, unit_price = $2, operation_value = $3 WHERE product = $4 AND date = $5', [new_quantity, new_unit_price, new_operation_value, item.product, row.date]);
+      }
+    }));
+
+    // delete the rows with movement Desdobro
+    await client.query('DELETE FROM finance_data WHERE movement = \'Desdobro\'');
+  }
+
+  // select the products and quantity where movement is Atualização
+  let products_with_atualizacao = await client.query('SELECT product, quantity FROM finance_data WHERE movement = \'Atualização\'');
+  products_with_atualizacao = products_with_atualizacao.rows;
+  if (products_with_atualizacao.length > 0) {
+    // for each row in products_with_atualizacao, get the other products that have the same quantity
+    let products_with_atualizacao_with_quantity = await Promise.all(products_with_atualizacao.map(async function(item: any) {
+      // select product from finance_data having sum of quantity group by product equals the quantity of the item
+      let products = await client.query('SELECT split_part(product::text, \' \', 1) FROM finance_data WHERE movement = \'Transferência - Liquidação\' GROUP BY split_part(product::text, \' \', 1) HAVING SUM(quantity) = $1', [item.quantity]);
+      // let products = await client.query('SELECT product FROM finance_data WHERE quantity = $1', [item.quantity]);
+      products = products.rows;
+      // products equals a list with the value of each key from products
+      products = products.map(function(item2: any) {
+        return item2.split_part;
+      });
+      return {
+        product: item.product,
+        quantity: item.quantity,
+        products: products
+      }
+    }));
+
+    // for each product in products_with_atualizacao_with_quantity, try to find the historical data in yahoo finance
+    await Promise.all(products_with_atualizacao_with_quantity.map(async function(item: any) {
+      // create a for loop to get the historical data of the product
+      for (let i = 0; i < item.products.length; i++) {
+        let queryOptions = { period1: '2021-01-01'};
+        try {
+          let quotes = await yahooFinance.historical((item.products[i].split(' ')[0] + '.SA'), queryOptions);
+        } catch (err) {
+          // if the product is not found in yahoo finance, change all the items with the product like the product in the item
+          await client.query('UPDATE finance_data SET product = $1 WHERE product LIKE $2', [item.product, item.products[i] + '%']);
+          i = item.products.length;
+        }
+      }
+    }));
+
+    // delete the rows with movement Atualização
+    await client.query('DELETE FROM finance_data WHERE movement = \'Atualização\'');
+      
+  }
+
+  return response.json({ message: 'Hello World' })
+})
+
+// create a route that calls the function transformFinanceData with a json as input
+routes.post('/transform-finance-data', async function(request: any, response: any) {
+  let newdata = await manage_data.transformFinanceData(request.body.financeData, request.body.withDividends);
+  return response.json(newdata);
+})
+
+
+routes.post('/posts', (request, response) => {
+  return response.json({ message: 'New post' })
+})
+
+export default routes
---
+++ src/utils/manage_data.ts
@@ -0,0 +1,421 @@
+var googleFinance = require('google-finance');
+const { Client } = require('pg')
+const multer = require('multer');
+var XLSX = require('node-xlsx');
+const yahooFinance = require('yahoo-finance2').default;
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'financedb',
+    password: 'ggi2011',
+    port: 5432,
+})
+client.connect()
+
+// create an async function
+export async function getFinancialData() {
+    let finance_data_json = await client.query('SELECT * FROM finance_data');
+    // convert to json
+    finance_data_json = finance_data_json.rows;
+
+    // get all the products names, date, quantity and unit price where movement is 'Transferência - Liquidação'
+    let stocks_bought = finance_data_json.filter(function (item: any) {
+        return item.movement === 'Transferência - Liquidação'
+    })
+    stocks_bought = stocks_bought.map(function (item: any) {
+        return {
+            product: item.product,
+            date: item.date,
+            quantity: item.quantity,
+            unit_price: item.unit_price
+        }
+    });
+
+    // transform the item name to only the string before the first space
+    stocks_bought = stocks_bought.map(function (item: any) {
+        return {
+            product: item.product.split(' ')[0] + '.SA',
+            date: item.date,
+            quantity: item.quantity,
+            unit_price: item.unit_price
+        }
+    });
+
+    // for item in stocks_bought, get the historical price
+    let stocks_bought_with_price = await Promise.all(stocks_bought.map(async function (item: any) {
+        let queryOptions = { period1: item.date };
+        let historical_price = 0;
+        try {
+            historical_price = await yahooFinance.historical(item.product, queryOptions);
+        }
+        catch (err) {
+            console.log(err);
+        }
+        return {
+            product: item.product,
+            type: 'Stock',
+            date: item.date,
+            quantity: item.quantity,
+            unit_price: item.unit_price,
+            history: historical_price
+        }
+    }));
+
+    // select from finance_data table where movement is 'Compra'
+    let stocks_sold = finance_data_json.filter(function (item: any) {
+        return item.movement === 'Compra'
+    })
+    stocks_sold = stocks_sold.map(function (item: any) {
+        return {
+            product: item.product,
+            type: 'Bond',
+            date: item.date,
+            quantity: item.quantity,
+            unit_price: item.unit_price,
+            history: [{"date": new Date(Number(item.product.slice(-4)), 0, 1, 0, 0, 0, 0), "close": 1000}]
+        }
+    });
+
+    // add the stocks sold to the stocks bought
+    stocks_bought_with_price = stocks_bought_with_price.concat(stocks_sold);
+
+    return stocks_bought_with_price
+}
+
+export function filterData(stocks_bought_with_price: any) {
+    stocks_bought_with_price = stocks_bought_with_price.map(function (item: any) {
+        // for each element in item delete fields open, high, low, volume, adjClose
+        for (let i = 0; i < item.history.length; i++) {
+            delete item.history[i].open;
+            delete item.history[i].high;
+            delete item.history[i].low;
+            delete item.history[i].volume;
+            delete item.history[i].adjClose;
+        }
+        return item;
+    });
+
+    return stocks_bought_with_price
+}
+
+
+export async function getFinancialDividends() {
+    let finance_data_json = await client.query('SELECT * FROM finance_data');
+    // convert to json
+    finance_data_json = finance_data_json.rows;
+
+    // get all the products names, date, quantity and unit price where movement is 'Transferência - Liquidação'
+    let stocks_bought = finance_data_json.filter(function (item: any) {
+        return item.movement === 'Dividendo' || item.movement === 'Juros Sobre Capital Próprio' || item.movement === 'Rendimento'
+    })
+    stocks_bought = stocks_bought.map(function (item: any) {
+        return {
+            product: item.product.split(' ')[0] + '.SA',
+            date: item.date,
+            quantity: item.quantity,
+            unit_price: item.unit_price
+        }
+    });
+
+    return stocks_bought
+}
+
+
+export function getDatesInRange(startDate: Date, endDate: Date) {
+    const date = new Date(startDate.getTime());
+  
+    const dates = [];
+  
+    while (date <= endDate) {
+      dates.push(String(new Date(date)));
+      date.setDate(date.getDate() + 1);
+    }
+  
+    return dates;
+}
+
+export function countDaysBetweenDates(startDate: Date, endDate: Date) {
+    const date = new Date(startDate.getTime());
+    let count = 0;
+    while (date <= endDate) {
+        count++;
+        date.setDate(date.getDate() + 1);
+    }
+    return count;
+}
+
+export async function transformFinanceData(financeData: any, withDividends: boolean) {
+    // for each element in data with product containing 'Tesouro Prefixado'
+    for (let i = 0; i < financeData.length; i++) {
+        if (financeData[i].product.includes('Tesouro Prefixado')) {
+            // add to the history of financeData[i] all the dates between the first and last date of financeData[i]
+            let datesTotal = getDatesInRange(new Date(financeData[i].date), new Date(financeData[i].history[0].date));
+            let dates = getDatesInRange(new Date(financeData[i].date), new Date());
+            // get the count of dates between the first and last date of data[i]
+            let countTotal = datesTotal.length;
+            let count = dates.length;
+            let buy_price = financeData[i].unit_price * financeData[i].quantity;
+            let last_day_value = 1000 * financeData[i].quantity;
+            let total_profit = last_day_value - buy_price;
+            let profit_per_day = total_profit / countTotal;
+            financeData[i].quantity = 1;
+            financeData[i].unit_price = buy_price;
+            // drop the first element from the history of financeData[i]
+            financeData[i].history.shift();
+            for (let j = 1; j < count - 1; j++) {
+                financeData[i].history.push(
+                    {
+                        date: new Date(dates[j]).toISOString().substring(0, 10),
+                        close: buy_price + (profit_per_day * j)
+                    }
+                )
+            }
+        }
+    }
+
+    for (let i = 0; i < financeData.length; i++) {
+        if (financeData[i].type == 'Stock') {
+            financeData[i].history.unshift(
+                {
+                    date: new Date(financeData[i].date).toISOString().substring(0, 10),
+                    close: Number(financeData[i].unit_price)
+                }
+            )
+        }
+    }
+
+    if (withDividends) {
+        let dividends = await getFinancialDividends();
+        for (let i = 0; i < dividends.length; i++) {
+            let index: any;
+            for (let j = 0; j < financeData.length; j++) {
+                if (financeData[j].product == dividends[i].product) {
+                    index = j;
+                    break;
+                }
+            }
+            // select the date of the financeData[index] before the dividends[i].date
+            for (let j = financeData[index].history.length - 1; j > 0; j--) {
+                if (new Date(financeData[index].history[j].date) <= new Date(dividends[i].date)) {
+                    // add to the history of financeData[index] the dividends[i]
+                    for (let k = j; k < financeData[index].history.length; k++) {
+                        if (new Date(financeData[index].history[k].date) >= new Date(financeData[index].history[k - 1].date)) {
+                            financeData[index].history[k].close = financeData[index].history[k].close + ((dividends[i].unit_price * dividends[i].quantity) / financeData[index].quantity);
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    const data: any = financeData.map((element: { history: any; quantity: any; unit_price: any; product: any;}) => {
+        if (element.history != 0) {
+            return element.history.map((element2: { date: any; close: any; }) => {
+                return {
+                    product: element.product,
+                    date: element2.date,
+                    quantity: element.quantity,
+                    close: element2.close,
+                    total_value: element.quantity * element2.close,
+                    delta:  (element2.close * element.quantity) - (element.quantity * element.unit_price)
+                }
+            })
+        }
+    });
+
+    let datesStored: any = [];
+    data.forEach((element: any) => {
+        element.forEach((element2: any) => {
+            if (!datesStored.includes(element2.date)) {
+                datesStored.push(String(element2.date));
+            }
+        })
+    });
+
+    // order datesStored
+    datesStored = datesStored.sort((a: any, b: any) => {
+        return new Date(a).getTime() - new Date(b).getTime();
+    });
+
+    let allDates: any = getDatesInRange(new Date(datesStored[0]), new Date(datesStored[datesStored.length - 1]));
+    // sort allDates
+    allDates = allDates.sort((a: any, b: any) => {
+        return new Date(a).getTime() - new Date(b).getTime();
+    });
+
+    // delete from allDates the dates after current time
+    allDates = allDates.filter((element: any) => {
+        return new Date(element).getTime() <= new Date().getTime();
+    });
+
+    let dataCopy = JSON.parse(JSON.stringify(data));
+    allDates.forEach((element: any) => {
+        for (let i = 0; i < data.length; i++) {
+            for (let j = 0; j < data[i].length; j++) {
+                if (
+                    new Date(String(element)) > new Date(String(data[i][0].date)) &&
+                    new Date(String(element)) < new Date(String(data[i][data[i].length - 1].date))
+                ) {
+                    if (
+                        new Date(String(data[i][j].date)) < new Date(String(element)) &&
+                        new Date(String(data[i][j + 1].date)) > new Date(String(element))
+                    ) {
+                        let firstDay = new Date(String(data[i][j].date));
+                        let element_date: any = new Date(String(element));
+                        let next_date: any = new Date(String(data[i][j + 1].date));
+                        let countTotalDays = countDaysBetweenDates(firstDay, next_date) - 1;
+                        let countDays = countDaysBetweenDates(element_date, next_date) - 1;
+                        let close = data[i][j].close + 
+                            (((data[i][j + 1].close - data[i][j].close) / countTotalDays) 
+                            * (countTotalDays - countDays));
+                        let delta = data[i][j].delta + 
+                            (((data[i][j + 1].delta - data[i][j].delta) / countTotalDays) 
+                            * (countTotalDays - countDays));
+                        dataCopy[i].push({
+                            product: data[i][j].product,
+                            quantity: data[i][j].quantity,
+                            date: element,
+                            close: close,
+                            total_value: data[i][j].quantity * close,
+                            delta: delta
+                        });
+                    }
+                }
+            }
+        }
+    });
+    
+    // map each date field inside each array in dataCopy
+    dataCopy.forEach((element: any) => {
+        element.forEach((element2: any) => {
+            element2.date = String(new Date(String(element2.date)));
+        });
+    });
+
+    for (let i = 0; i < dataCopy.length; i++) {
+        dataCopy[i] = dataCopy[i].filter((element: { date: any; }) => {
+            return element.date.substring(8, 10) === '01'
+        })
+    }
+
+    let newDatesStored: any = [];
+    dataCopy.forEach((element: any) => {
+        element.forEach((element2: any) => {
+            if (!newDatesStored.includes(element2.date)) {
+                newDatesStored.push(String(new Date(String(element2.date))));
+            }
+        })
+    });
+    newDatesStored = newDatesStored.sort((a: any, b: any) => {
+        return new Date(a).getTime() - new Date(b).getTime();
+    });
+    // sort all arrays in dataCopy by date
+    dataCopy.forEach((element: any) => {
+        element.sort((a: any, b: any) => {
+            return new Date(a.date).getTime() - new Date(b.date).getTime();
+        });
+    });
+
+    // add a new element to dataCopy with the sum of the close values for each date in newDatesStored
+    let newData: any = [];
+    newDatesStored.forEach((element: any) => {
+        let sum = 0;
+        let deltaSum = 0;
+        let quantitySum = 0;
+        let totalValueSum = 0;
+        dataCopy.forEach((element2: any) => {
+            element2.forEach((element3: any) => {
+                if (element3.date === element) {
+                    sum += element3.close;
+                    deltaSum += element3.delta;
+                    quantitySum += element3.quantity;
+                    totalValueSum += element3.total_value;
+                }
+            })
+        });
+        newData.push({
+            product: 'Total',
+            date: element,
+            quantity: quantitySum,
+            close: sum,
+            total_value: totalValueSum,
+            delta: deltaSum
+        })
+    });
+    dataCopy.push(newData);
+
+    // filter dataCopy to remove empty arrays
+    dataCopy = dataCopy.filter((element: any) => {
+        return element.length > 0;
+    });
+
+    // create a new array that sums the values from the dataCopy arrays that are from the same product
+    let newData2: any = [];
+    dataCopy.forEach((element: any) => {
+        let newProductArray: any = [];
+        let product_name = element[0].product;
+        // if none of the elements in the newData2 contains a product named the same as the current element
+        if (newData2.length == 0 || !newData2.some((element2: any) => {
+            return element2[0].product == product_name;
+        }
+        )) {
+            dataCopy.forEach((element2: any) => {
+                if (element2[0].product == product_name) {
+                    element2.forEach((element3: any) => {
+                        // if element3.date is already in newProductArray, sum element3.close and element3.delta to the value of the date
+                        if (newProductArray.includes(element3.date)) {
+                            newProductArray.forEach((element4: any) => {
+                                if (element4.date === element3.date) {
+                                    element4.quantity += element3.quantity;
+                                    element4.total_value += element3.total_value;
+                                    element4.delta += element3.delta;
+                                }
+                            })
+                        } else {
+                            newProductArray.push(element3);
+                        }
+                    });
+                }
+            });
+            newData2.push(newProductArray);
+        }
+    });
+
+    // for each array in newData2
+    let newData3: any = [];
+    newData2.forEach((element: any) => {
+        // push to newData3 an array with the same elements as element, but with the sum of the close values for each date
+        let newArray: any = [];
+        let insertedDates: any = [];
+        // for each element in element
+        element.forEach((element2: any) => {
+            // if element2.date is not in insertedDates
+            if (!insertedDates.includes(element2.date)) {
+                // push element2 to newArray
+                insertedDates.push(element2.date);
+                newArray.push(element2);
+            } else {
+                // sum element2.close and element2.delta to the value of the date in newArray
+                newArray.forEach((element3: any) => {
+                    if (element3.date === element2.date) {
+                        element3.quantity += element2.quantity;
+                        element3.total_value += element2.total_value;
+                        element3.delta += element2.delta;
+                    }
+                });
+            }
+        });
+        newData3.push(newArray);
+    });
+
+    // order newData3 by date
+    newData3.forEach((element: any) => {
+        element.sort((a: any, b: any) => {
+            return new Date(a.date).getTime() - new Date(b.date).getTime();
+        });
+    });
+
+    return [newDatesStored, newData3];
+}
